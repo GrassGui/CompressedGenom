970697272aa1de2436fccf269a5c31d27b87d2ac
GrassGui <agatakoltunova9@gmail.com>
2026-04-03 09:06:38 +0300

    test

---
+++ src/compgene/UnitTypes.java
@@ -0,0 +1,61 @@
+package mindustry.content;
+
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.math.geom.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.ai.*;
+import mindustry.ai.types.*;
+import mindustry.annotations.Annotations.*;
+import mindustry.entities.*;
+import mindustry.entities.abilities.*;
+import mindustry.entities.bullet.*;
+import mindustry.entities.effect.*;
+import mindustry.entities.part.*;
+import mindustry.entities.pattern.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.type.ammo.*;
+import mindustry.type.unit.*;
+import mindustry.type.weapons.*;
+import mindustry.world.meta.*;
+
+import static arc.graphics.g2d.Draw.*;
+import static arc.graphics.g2d.Lines.*;
+import static arc.math.Angles.*;
+import static mindustry.Vars.*;
+
+public class UnitTypes{
+    //region standard
+
+    //mech
+    public static @EntityDef({Unitc.class, Mechc.class}) UnitType firefly;
+
+    public static void load(){
+        //region ground attack
+
+        firefly = new UnitType("firefly"){{
+            researchCostMultiplier = 0.5f;
+            speed = 0.5f;
+            hitSize = 8f;
+            health = 150;
+            stepSoundVolume = 0.4f;
+
+            weapons.add(new Weapon("large-weapon"){{
+                reload = 13f;
+                x = 4f;
+                y = 2f;
+                top = false;
+                ejectEffect = Fx.casing1;
+                bullet = new BasicBulletType(2.5f, 9){{
+                    width = 7f;
+                    height = 9f;
+                    lifetime = 60f;
+                }};
+            }});
+        }};
+    }
+}
